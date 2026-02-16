477cad92fa9e0db6e7c49c095b6c7b246c05fe33
RyanAsif27 <i41462558@gmail.com>
2026-02-16 20:14:33 +0500

    New file with full code for hangman

---
+++ list_of_words_hangman.py
@@ -0,0 +1,42 @@
+words = [
+    # Fruits
+    "apple", "banana", "orange", "grape", "mango", "pineapple", "strawberry",
+    "watermelon", "kiwi", "peach", "cherry", "papaya", "apricot", "blueberry",
+    "raspberry", "blackberry", "melon", "lemon", "lime",
+
+    # Animals
+    "cat", "dog", "elephant", "tiger", "lion", "giraffe", "monkey", "zebra",
+    "kangaroo", "panda", "bear", "wolf", "fox", "rabbit", "deer", "cow",
+    "sheep", "goat", "horse", "duck", "owl", "eagle", "penguin", "dolphin",
+    "shark", "whale", "crocodile", "alligator", "hippopotamus",
+
+    # Countries / Cities
+    "paris", "london", "tokyo", "berlin", "madrid", "rome", "canada", "brazil",
+    "china", "india", "egypt", "russia", "germany", "italy", "spain",
+    "australia", "mexico", "japan", "france", "norway", "sweden", "denmark",
+    "finland", "argentina", "chile", "portugal", "greece", "ireland", "poland",
+
+    # Objects
+    "chair", "table", "lamp", "door", "window", "pencil", "notebook", "book",
+    "phone", "computer", "bag", "shoe", "hat", "bottle", "cup", "plate",
+    "fork", "spoon", "knife", "clock", "watch", "mirror", "key", "wallet",
+
+    # Nature / Weather
+    "sun", "moon", "star", "cloud", "rain", "snow", "storm", "wind", "thunder",
+    "lightning", "fog", "river", "mountain", "valley", "forest", "desert",
+    "beach", "ocean", "lake", "tree", "flower", "grass", "leaf", "stone",
+
+    # Colors
+    "red", "blue", "green", "yellow", "purple", "black", "white", "orange",
+    "pink", "brown", "gray", "violet", "indigo", "gold", "silver",
+
+    # Food / Drinks
+    "bread", "rice", "pasta", "cheese", "butter", "milk", "tea", "coffee",
+    "soup", "salad", "pizza", "burger", "sandwich", "cake", "chocolate",
+    "icecream", "honey", "yogurt", "juice", "water", "soda",
+
+    # Miscellaneous
+    "school", "teacher", "student", "family", "friend", "music", "movie",
+    "song", "game", "party", "holiday", "travel", "garden", "market",
+    "station", "airport", "library", "hospital", "museum", "theater"
+]
